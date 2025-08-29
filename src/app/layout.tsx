acc1486c80887c80a251550fe1f35b74c3e607ca
Cleaning Professionals <account@cleaningprofessionals.com.au>
2025-08-29 14:43:11 +1000

    Version 1 Deploy 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
 	title: 'POSS Admin',
 	description: 'Simple POS & bookkeeping admin dashboard',
+	icons: {
+		icon: '/favicon.ico',
+	},
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
